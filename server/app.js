7482642f11f9f417e2e882ec6cf1393a4a03fb71
Wojtek-A <wojtek.a@hotmail.com>
2023-05-24 21:30:52 +0200

    start

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 
 app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from server TEST!' });
+  res.json({ message: 'Hello from node server! TEST!' });
 });
 
 app.use((req, res) => {
