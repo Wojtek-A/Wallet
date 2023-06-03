5cd04fc021f5e9d3f28c0760d7382bf9decd81d0
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2023-06-03 21:44:24 +0200

    Deleted comments

--- client/src/redux/wallet/wallet.thunk.js
+++ client/src/redux/wallet/wallet.thunk.js
@@ -6,7 +6,6 @@ export const fetchTransactions = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const res = await axios.get("/transactions");
-      // setAuthHeader(res.data.token);
       console.log("Response data:", res.data);
       return res.data.data; //TODO poprawić strukturę endpointa
     } catch (e) {
@@ -20,7 +19,6 @@ export const addTransaction = createAsyncThunk(
   async (newTransaction, thunkAPI) => {
     try {
       const res = await axios.post("/transactions", newTransaction);
-      // setAuthHeader(res.data.token);
       return res.data.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
@@ -33,7 +31,6 @@ export const deleteTransaction = createAsyncThunk(
   async (transactionId, thunkAPI) => {
     try {
       const res = await axios.delete(`/transactions/${transactionId}`);
-      // setAuthHeader(res.data.token);
       return res.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
@@ -53,7 +50,7 @@ export const updateTransaction = createAsyncThunk(
         comment,
         category,
       });
-      // setAuthHeader(res.data.token);
+
       const { transaction } = res.data;
       return { transaction, state };
     } catch (error) {
