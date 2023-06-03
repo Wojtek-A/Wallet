df3bb700446e76dbea36b68f454688fbc3bd4fea
AdamczukPawel <104322297+AdamczukPawel@users.noreply.github.com>
2023-06-03 21:43:07 +0200

    Fix 401 Unauthorized bug and Username in Header bug

--- server/controllers/auth/loginController.js
+++ server/controllers/auth/loginController.js
@@ -1,32 +1,32 @@
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import { User } from '../../models/Users.js';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { User } from "../../models/Users.js";
 
 const login = async (req, res, _) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    const compareResult = user && await bcrypt.compare(password, user.password);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  const compareResult = user && (await bcrypt.compare(password, user.password));
 
-    if (!user || !compareResult) {
-        res.status(401).json({ message: 'Email or password is wrong' });
-        return;
-    }
-    const payload = {
-        id: user._id,
-    };
-    const { SECRET } = process.env;
+  if (!user || !compareResult) {
+    res.status(401).json({ message: "Email or password is wrong" });
+    return;
+  }
+  const payload = {
+    id: user._id,
+  };
+  const { SECRET } = process.env;
 
-    const token = jwt.sign(payload, SECRET, { expiresIn: '1h' });
+  const token = jwt.sign(payload, SECRET, { expiresIn: "1h" });
 
-    await User.findByIdAndUpdate(user._id, { token });
-    res.status(201).json({
-        token: token,
-        user: {
-            email: user.email,
-            name: user.name,
-            id: user._id,
-        },
-    });
+  await User.findByIdAndUpdate(user._id, { token });
+  res.status(201).json({
+    token: token,
+    user: {
+      email: user.email,
+      username: user.username,
+      id: user._id,
+    },
+  });
 };
 
 export default login;
