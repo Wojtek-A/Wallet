a294c411d7d36c1cb55cd7d9e95dfa9686dc6903
MateuszJaz <mateusz.jaz@outlook.com>
2023-06-05 14:04:07 +0200

    transactionList style done / added custom loader

--- client/src/components/TransactionList/TransactionList.jsx
+++ client/src/components/TransactionList/TransactionList.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { selectTransactions } from "../../redux/selector";
+import { selectIsLoading, selectTransactions } from "../../redux/selector";
 import {
   changeIsModalEditTrasactionOpen,
   setTransactionToEdit,
@@ -12,9 +12,11 @@ import {
 import css from "./TransactionList.module.css";
 import clsx from "clsx";
 import sprite from "../../assets/icon/sprite.svg";
+import Loader from "../Loader/Loader";
 
 const TransactionList = () => {
   const transactions = useSelector(selectTransactions);
+  const isLoading = useSelector(selectIsLoading);
 
   const dispatch = useDispatch();
 
@@ -24,63 +26,77 @@ const TransactionList = () => {
   };
 
   return (
-    <div className={css.container}>
-      <table className={css.transactionTable}>
-        <thead className={css.tableHead}>
-          <tr>
-            <th className={css.tableHeader}>Date</th>
-            <th className={css.tableHeader}>Type</th>
-            <th className={css.tableHeader}>Category</th>
-            <th className={css.tableHeader}>Comment</th>
-            <th className={css.tableHeader}>Sum</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {transactions.map((transaction, index) => (
-            <tr key={index}>
-              <td className={css.tableData}>
-                {new Date(transaction.date).toLocaleDateString()}
-              </td>
-              <td className={css.tableData}>{transaction.type ? "+" : "-"}</td>
-              <td className={css.tableData}>{transaction.category}</td>
-              <td className={css.tableData}>{transaction.comment}</td>
-              <td
-                className={clsx(css.tableData, {
-                  [css.plus]: transaction.type,
-                  [css.minus]: !transaction.type,
-                })}
-              >
-                {transaction.amount}
-              </td>
-              <td className={css.tableData}>
-                <div className={css.wrapper}>
-                  <button
-                    className={css.btnEdit}
-                    id={transaction._id}
-                    onClick={() => openModalEditTransaction(transaction)}
-                  >
-                    <svg className={css.icon}>
-                      <use xlinkHref={`${sprite}#pen`}></use>
-                    </svg>
-                  </button>
-                  <button
-                    className={css.btn}
-                    onClick={() => {
-                      dispatch(deleteTransaction(transaction._id)).then(() =>
-                        dispatch(fetchTransactions())
-                      );
-                    }}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </td>
+    <>
+      <div className={css.container}>
+        <table className={css.transactionTable}>
+          <thead className={css.tableHead}>
+            <tr>
+              <th className={css.tableHeader}>Date</th>
+              <th className={css.tableHeader}>Type</th>
+              <th className={css.tableHeader}>Category</th>
+              <th className={css.tableHeader}>Comment</th>
+              <th className={css.tableHeader}>Sum</th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          {isLoading ? (
+            <tbody>
+              <tr>
+                <td>
+                  <Loader variant={"wallet"} />
+                </td>
+              </tr>
+            </tbody>
+          ) : (
+            <tbody>
+              {transactions.map((transaction, index) => (
+                <tr key={index}>
+                  <td className={css.tableData}>
+                    {new Date(transaction.date).toLocaleDateString()}
+                  </td>
+                  <td className={css.tableData}>
+                    {transaction.type ? "+" : "-"}
+                  </td>
+                  <td className={css.tableData}>{transaction.category}</td>
+                  <td className={css.tableData}>{transaction.comment}</td>
+                  <td
+                    className={clsx(css.tableData, {
+                      [css.plus]: transaction.type,
+                      [css.minus]: !transaction.type,
+                    })}
+                  >
+                    {transaction.amount}
+                  </td>
+                  <td className={css.tableData}>
+                    <div className={css.wrapper}>
+                      <button
+                        className={css.btnEdit}
+                        id={transaction._id}
+                        onClick={() => openModalEditTransaction(transaction)}
+                      >
+                        <svg className={css.icon}>
+                          <use xlinkHref={`${sprite}#pen`}></use>
+                        </svg>
+                      </button>
+                      <button
+                        className={css.btn}
+                        onClick={() => {
+                          dispatch(deleteTransaction(transaction._id)).then(
+                            () => dispatch(fetchTransactions())
+                          );
+                        }}
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          )}
+        </table>
+      </div>
+    </>
   );
 };
 
